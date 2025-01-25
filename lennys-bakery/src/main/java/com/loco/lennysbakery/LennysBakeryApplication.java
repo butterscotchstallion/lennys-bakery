939e252ebd2d715a31748c3a11a3439aeda30263
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-01-25 09:53:34 -0500

    Fixes data source URL error

--- lennys-bakery/src/main/java/com/loco/lennysbakery/LennysBakeryApplication.java
+++ lennys-bakery/src/main/java/com/loco/lennysbakery/LennysBakeryApplication.java
@@ -3,17 +3,16 @@ package com.loco.lennysbakery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LennysBakeryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LennysBakeryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LennysBakeryApplication.class, args);
+    }
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 }
