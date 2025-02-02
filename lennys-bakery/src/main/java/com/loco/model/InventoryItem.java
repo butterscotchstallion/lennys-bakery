ae43c781985abeefcbe69edc1d33a544a13bc7a9
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-02-01 22:33:55 -0500

    Adds Flyway, additional columns

--- lennys-bakery/src/main/java/com/loco/model/InventoryItem.java
+++ lennys-bakery/src/main/java/com/loco/model/InventoryItem.java
@@ -2,13 +2,18 @@ package com.loco.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.time.Instant;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.ColumnDefault;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "inventory", schema = "public")
+@Table(name = "inventory_item", schema = "public")
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -16,21 +21,42 @@ import lombok.Setter;
 public class InventoryItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "inventory_item_id_gen", sequenceName = "inventory_item_id_seq")
+    @Column(name = "id", nullable = false)
     private long id;
 
+    @Size(max = 200)
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Size(max = 200)
+    @Column(name = "short_description", nullable = false)
+    private String shortDescription;
+
+    @Size(max = 1000)
     @Column(name = "description", nullable = false)
     private String description;
 
     @Column(name = "price", nullable = false)
     private BigDecimal price;
 
-    public InventoryItem(String name, String description, BigDecimal price) {
+    @NotNull
+    @ColumnDefault("now()")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "image_filename")
+    private String imageFilename;
+
+    public InventoryItem(String name, String description, String shortDescription, BigDecimal price, String imageFilename) {
         this.name = name;
         this.description = description;
         this.price = price;
+        this.imageFilename = imageFilename;
+        this.shortDescription = shortDescription;
     }
 
     @Override
