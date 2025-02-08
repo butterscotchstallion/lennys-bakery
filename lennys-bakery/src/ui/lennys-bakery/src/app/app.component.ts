391e930c0cf6bc1956d104ffa795bb09b3192a3a
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-02-08 17:03:53 -0500

    Adds search box and delivery area

--- lennys-bakery/src/ui/lennys-bakery/src/app/app.component.ts
+++ lennys-bakery/src/ui/lennys-bakery/src/app/app.component.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import {
-  faCartPlus,
-  faCartShopping,
-  faChevronDown,
-} from '@fortawesome/free-solid-svg-icons';
-import { CartComponent } from './components/cart/CartComponent';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-import { UserMenuComponent } from './components/user/UserComponent';
+    faCartPlus,
+    faCartShopping,
+    faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { CartComponent } from "./components/cart/CartComponent";
+import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
+import { UserMenuComponent } from "./components/user/UserComponent";
+import { InputText } from "primeng/inputtext";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    FontAwesomeModule,
-    CartComponent,
-    ToastModule,
-    UserMenuComponent,
-  ],
-  providers: [MessageService],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: "app-root",
+    imports: [
+        RouterOutlet,
+        FontAwesomeModule,
+        CartComponent,
+        ToastModule,
+        UserMenuComponent,
+        InputText,
+    ],
+    providers: [MessageService],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'lennys-bakery';
-  protected readonly faCartPlus = faCartPlus;
-  protected readonly faCartShopping = faCartShopping;
-  protected readonly faChevronDown = faChevronDown;
+    title = "lennys-bakery";
+    protected readonly faCartPlus = faCartPlus;
+    protected readonly faCartShopping = faCartShopping;
+    protected readonly faChevronDown = faChevronDown;
 }
