67d7bcd7696d150aa7e7ce86f951a080608f3690
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-02-08 17:25:48 -0500

    Refactors header styles a bit and adds product name to toast

--- lennys-bakery/src/ui/lennys-bakery/src/app/components/product/ProductComponent.ts
+++ lennys-bakery/src/ui/lennys-bakery/src/app/components/product/ProductComponent.ts
@@ -1,68 +1,68 @@
-import { Component, DestroyRef, inject, Input } from '@angular/core';
-import { NgClass, NgOptimizedImage } from '@angular/common';
-import { Button } from 'primeng/button';
+import { Component, DestroyRef, inject, Input } from "@angular/core";
+import { NgClass, NgOptimizedImage } from "@angular/common";
+import { Button } from "primeng/button";
 import {
-  FaIconComponent,
-  IconDefinition,
-} from '@fortawesome/angular-fontawesome';
-import { faCartPlus, faComments } from '@fortawesome/free-solid-svg-icons';
-import { IProduct } from '../../models/IProduct';
-import { CartService } from '../../services/CartService';
-import { IAddToCartItem } from '../../models/IAddToCartItem';
-import { MessageService } from 'primeng/api';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ICart } from '../../models/ICart';
+    FaIconComponent,
+    IconDefinition,
+} from "@fortawesome/angular-fontawesome";
+import { faCartPlus, faComments } from "@fortawesome/free-solid-svg-icons";
+import { IProduct } from "../../models/IProduct";
+import { CartService } from "../../services/CartService";
+import { IAddToCartItem } from "../../models/IAddToCartItem";
+import { MessageService } from "primeng/api";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { ICart } from "../../models/ICart";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './productCardComponent.html',
-  imports: [NgOptimizedImage, Button, FaIconComponent, NgClass],
-  styleUrls: ['./productCardStyles.scss'],
+    selector: "app-product",
+    templateUrl: "./productCardComponent.html",
+    imports: [NgOptimizedImage, Button, FaIconComponent, NgClass],
+    styleUrls: ["./productCardStyles.scss"],
 })
 export class ProductComponent {
-  @Input() product: IProduct;
-  @Input() cartMap: Map<number, ICart>;
-  isLoading = false;
-  protected readonly faComments: IconDefinition = faComments;
-  protected readonly faCartPlus: IconDefinition = faCartPlus;
-  private destroyRef: DestroyRef = inject(DestroyRef);
+    @Input() product: IProduct;
+    @Input() cartMap: Map<number, ICart>;
+    isLoading = false;
+    protected readonly faComments: IconDefinition = faComments;
+    protected readonly faCartPlus: IconDefinition = faCartPlus;
+    private destroyRef: DestroyRef = inject(DestroyRef);
 
-  constructor(
-    private cartService: CartService,
-    private messageService: MessageService,
-  ) {}
+    constructor(
+        private cartService: CartService,
+        private messageService: MessageService,
+    ) {}
 
-  getCartQuantity(productId: number): string {
-    const cartItem: ICart | undefined = this.cartMap.get(productId);
-    return cartItem ? cartItem.quantity.toString() : '0';
-  }
+    getCartQuantity(productId: number): string {
+        const cartItem: ICart | undefined = this.cartMap.get(productId);
+        return cartItem ? cartItem.quantity.toString() : "0";
+    }
 
-  addToCart(product: IProduct) {
-    this.isLoading = true;
-    const cartItem: IAddToCartItem = {
-      inventoryItemId: product.id,
-      quantity: 1,
-    };
-    this.cartService
-      .addItemToCart(cartItem)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: () => {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Success',
-            detail: 'Item added to cart',
-          });
-          this.isLoading = false;
-        },
-        error: () => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Failed to add item to cart',
-          });
-          this.isLoading = false;
-        },
-      });
-  }
+    addToCart(product: IProduct) {
+        this.isLoading = true;
+        const cartItem: IAddToCartItem = {
+            inventoryItemId: product.id,
+            quantity: 1,
+        };
+        this.cartService
+            .addItemToCart(cartItem)
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe({
+                next: () => {
+                    this.messageService.add({
+                        severity: "success",
+                        summary: "Success",
+                        detail: product.name + " added to cart",
+                    });
+                    this.isLoading = false;
+                },
+                error: () => {
+                    this.messageService.add({
+                        severity: "error",
+                        summary: "Error",
+                        detail: "Failed to add " + product.name + " to cart",
+                    });
+                    this.isLoading = false;
+                },
+            });
+    }
 }
