33d60a4361b7a6bade37fb6fc8c83287cbb839e2
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-02-16 23:15:54 -0500

    Fixes item form

--- lennys-bakery/src/ui/lennys-bakery/src/app/pages/Admin/AdminPageComponent.ts
+++ lennys-bakery/src/ui/lennys-bakery/src/app/pages/Admin/AdminPageComponent.ts
@@ -1,11 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, DestroyRef, inject } from "@angular/core";
 import { PageHeaderComponent } from "../../components/PageHeader/PageHeaderComponent";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { ProductService } from "../../services/ProductService";
 import { IProduct } from "../../models/IProduct";
-import { Subject } from "rxjs";
 import { AdminEditInventoryItemComponent } from "../../components/Admin/Inventory/Form/AdminEditInventoryItemComponent";
 import { CommonModule } from "@angular/common";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { catchError, throwError } from "rxjs";
+import { MessageService } from "primeng/api";
 
 @Component({
     selector: "app-admin-page",
@@ -18,15 +20,32 @@ import { CommonModule } from "@angular/common";
     ],
 })
 export class AdminPageComponent {
-    product$: Subject<IProduct>;
+    product: IProduct;
+    private destroyRef: DestroyRef = inject(DestroyRef);
 
     constructor(
         route: ActivatedRoute,
         private productService: ProductService,
+        private messageService: MessageService,
     ) {
         const itemSlug: string = route.snapshot.paramMap.get("slug");
         if (itemSlug) {
-            this.product$ = this.productService.getProductBySlug(itemSlug);
+            this.productService
+                .getProductBySlug(itemSlug)
+                .pipe(
+                    takeUntilDestroyed(this.destroyRef),
+                    catchError((_) => {
+                        this.messageService.add({
+                            severity: "error",
+                            summary: "Error",
+                            detail: "There was a problem fetching the product.",
+                        });
+                        return throwError(() => "Failed to load product");
+                    }),
+                )
+                .subscribe((product: IProduct) => {
+                    this.product = product;
+                });
         }
     }
 }
